d41c51327c152f1cda566b5f15e7ca46dbc065c5
Anya Morris <morrisanya202@gmail.com>
2025-08-06 09:54:50 -0500

    Conclusion, sources, Shakespeare

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 
 
 <h1 style="text-align:center">What can LLMs really do?</h1>
+<p style="text-align: center">By: Anya Morris</p>
 <br>
 
 <div class="section">
@@ -87,6 +88,21 @@
     let w_words = ["wine", "whisky", "hibiscus"]
     let a_bars = [55, 35, 10]
     let a_words = ["coffee", "tequila", "mocha"]
+    let s_words = ["stage", "game", "dream", "lie"]
+    let s_bars = [74, 11, 9, 6]
+
+    var i = 0;
+    var txt = 'I am going on a...';
+    var speed = 50;
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("demo").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+        
+    }
 </script>
 
 <div>
@@ -169,10 +185,12 @@
                 in order to decide which word is the next best fit.
             </p>
         </div>
-        <div>            
-            <p class="speech-bubble">
+        <div>   
+                    
+            <p class="speech-bubble" id="demo" style="height:30px">
                 <img src="./Human.png" alt="Human icon" style="border-radius: 3px; max-width: 30px; height: auto; float:left; margin-right: 10px;">
-                I am going on a...</p>
+               I am going on a...
+            </p>
         </div>
         <div class="response">
             <img src="./RobotIcon.png" alt="Robot icon" style="border-radius: 3px; max-width: 30px; height: auto; float:left; margin-right: 10px;">
@@ -205,17 +223,41 @@
     <h2>Capabilities</h2>
     
     <div class="section">
-        <h3>Summarization</h3>
+        <h3>Style Imitation</h3>
         <div>
-            <p>One of LLMs' most effective capabilities is text summarization. This makes
-                sense given what we now know about word predictions. It's much easier to 
-                predict the next most likely word when all the words are provided to the 
-                LLM through the piece that you want summarized. LLMs use two different 
-                summarization techniques. Either they extract sentences directly from the text 
-                or they create an abstract summary with their own creativity. Usually these
-                techniques are combined for the best results, but both use statistics to 
-                predict words. 
+            <p>
+                One of LLMs' most effective capabilities is style imitation. If an LLM has enough
+                data on the language of a certain person, time period, or genre, it can imitate the 
+                style of that thing. For example, ChatGPT has a lot of data on Shakespeare because
+                he is so prominent in English culture. Therefore, it is really good at recognizing
+                Shakespeare's work and imitating it. It can easily fill in his famous quotes, as 
+                shown in this output.
             </p>
+        </div>
+        <div>
+            <p class="speech-bubble">
+                <img src="./Shakespeare.png" alt="Shakespeare icon" style="border-radius: 3px; max-width: 30px; height: auto; float:left; margin-right: 10px;">
+                All the world's a...
+            </p>
+        </div>
+        <div class="response">
+            <img src="./RobotIcon.png" alt="Robot icon" style="border-radius: 3px; max-width: 30px; height: auto; float:left; margin-right: 10px;">
+            <svg width={500} height={200}>
+                {#each s_bars as d, i}
+                    <rect
+                    x= {0} 
+                    y={(i+1)*35} 
+                    height={30} 
+                    width={10*d} 
+                    fill="steelblue" 
+                    />
+            
+                    <text fill="#ffffff" font-size="12" font-family="Verdana" x="5" y={55 + (i*35)}>{d}%:  {s_words[i]}</text>     
+
+            
+                {/each}
+            </svg>
+        
         </div>
     </div>
 </div>
@@ -287,6 +329,84 @@
         </div>
     </div>
     
+
+</div>
+<div>
+    <h2>Conclusion</h2>
+
+    <div class="section">
+        <div>
+            <p>LLMs can be great tools using their vast data and statistical prediction
+            power. However, it is important to understand at least basically how they
+            work before determining what they should be used for. They are good at 
+            tasks like imitation, summarization, and easy explanations, but problems
+            arrive when those tasks are attempted when there are holes in the data set.
+            Always remember what bias might be worked into a model and how that affects
+            the task you are asking it to complete.
+            </p>
+        </div>
+
+    </div>
+</div>
+<div>
+    <h3>Sources</h3>
+
+    <p>
+        3Blue1Brown. (2024, April 1). But what is a GPT? Visual intro to Transformers | Deep learning, chapter 5. Www.youtube.com. https://www.youtube.com/watch?v=wjZofJX0v4M
+        <br>
+        Almeida, D., Shmarko, K., & Lomas, E. (2022). The ethics of facial recognition technologies, surveillance, and accountability in an age of artificial intelligence: a comparative analysis of US, EU, and UK regulatory frameworks. AI and Ethics, 2(3), 377–387. https://doi.org/10.1007/s43681-021-00077-w
+        <br>
+        Backlinko team. (2024, June 4). ChatGPT Statistics 2024: How Many People Use ChatGPT? Backlinko. https://backlinko.com/chatgpt-stats
+        <br>
+        Briganti, G. (2023). How ChatGPT works: a mini review. European Archives of Oto-Rhino-Laryngology, 281(3). https://doi.org/10.1007/s00405-023-08337-7
+        <br>
+        Buolamwini, J. (2023, November 8). “If you have a face, you have a place in the conversation about AI”, experts say (T. Mosley, Interviewer) [Interview]. In NPR.
+        <br>
+        Cho, A., Kim, G., Karpekov, A., Helbling, A., Wang, J., Lee, S., Hoover, B., & Chau, P. (2017). Transformer Explainer: LLM Transformer Model Visually Explained. Github.io. https://poloclub.github.io/transformer-explainer/
+        <br>
+        Coursera. (2023, January 12). 4 Types of AI: Getting to Know Artificial Intelligence. Coursera. https://www.coursera.org/articles/types-of-ai
+        <br>
+        Dorr, L. (2022). Types of Artificial Intelligence, Explained. Www.dentalproductsreport.com, 56. https://www.dentalproductsreport.com/view/types-of-artificial-intelligence-explained
+        <br>
+        Gherheş, V. (2018). Why Are We Afraid of Artificial Intelligence (Ai)? European Review of Applied Sociology, 11(17), 6–15. https://doi.org/10.1515/eras-2018-0006
+        <br>
+        Hartmann, V., Suri, A., Bindschaedler, V., Evans, D., Tople, S., & West, R. (2023). SoK: Memorization in General-Purpose Large Language Models. https://arxiv.org/pdf/2310.18362
+        <br>
+        Hayati, S., Lee, M., Rajagopal, D., & Kang, D. (2024). How Far Can We Extract Diverse Perspectives from Large Language Models? arXiv.
+        <br>
+        Howarth, J. (2024, August 6). Number of Parameters in GPT-4 (Latest Data). Exploding Topics; Exploding Topics. https://explodingtopics.com/blog/gpt-parameters
+        <br>
+        IBM Data and AI Team. (2023, October 12). Types of Artificial Intelligence | IBM. Www.ibm.com; IBM. https://www.ibm.com/think/topics/artificial-intelligence-types
+        <br>
+        Kim, Y., Lee, J., Kim, S., Park, J., & Kim, J. (2024). Understanding Users’ Dissatisfaction with ChatGPT Responses: Types, Resolving Tactics, and the Effect of Knowledge Level. ACM. https://doi.org/10.1145/3640543.3645148
+        <br>
+        Lee, S., Hoover, B., Strobelt, H., Wang, Z. J., Peng, S., Wright, A., Li, K., Park, H., Yang, H., & Chau, D. H. (2023, May 8). Diffusion Explainer: Visual Explanation for Text-to-image Stable Diffusion. ArXiv.org. https://doi.org/10.48550/arXiv.2305.03509
+        <br>
+        Li, J., Zhou, F., Sun, S., Zhang, Y., Zhao, H., & Liu, P. (2024). Dissecting Human and LLM Preferences. ArXiv, abs/2402.11296.
+        <br>
+        meta-llama. (2024). llama-models/models/llama3_1/MODEL_CARD.md at main · meta-llama/llama-models. GitHub. https://github.com/meta-llama/llama-models/blob/main/models/llama3_1/MODEL_CARD.md
+        <br>
+        Naik, D., Naik, I., & Naik, N. (2024). Large Data Begets Large Data: Studying Large Language Models (LLMs) and Its History, Types, Working, Benefits and Limitations. Lecture Notes in Networks and Systems, 293–314. https://doi.org/10.1007/978-3-031-74443-3_18
+        <br>
+        Naous, T., Ryan, M., Ritter, A., & Xu, W. (2024). Having Beer after Prayer? Measuring Cultural Bias in Large Language Models. https://arxiv.org/pdf/2305.14456
+        <br>
+        Pellegrino, M., & Kelly, R. (2019). Intelligent machines and the growing importance of ethics. JSTOR. https://www.jstor.org/stable/resrep19966.11
+        <br>
+        Prompt Engineering. (2025, June 7). Www.promptingguide.ai; DAIR.AI. https://www.promptingguide.ai/techniques/cot
+        <br>
+        Riemer, K., & Peter, S. (2024). Conceptualizing generative AI as style engines: Application archetypes and implications. International Journal of Information Management, 79, 102824–102824. https://doi.org/10.1016/j.ijinfomgt.2024.102824
+        <br>
+        Schepman, A., & Rodway, P. (2020). Initial validation of the general attitudes towards Artificial Intelligence Scale. Computers in Human Behavior Reports, 1. https://doi.org/10.1016/j.chbr.2020.100014
+        <br>
+        Shankar, S., Halpern, Y., Breck, E., Atwood, J., Wilson, J., & Sculley, D. (2017). No Classification without Representation: Assessing Geodiversity Issues in Open Data Sets for the Developing World. arXiv.
+        <br>
+        Townsend, E. (2025, March 12). Survey: 52% of U.S. adults now use AI large language models like ChatGPT. Today at Elon. https://www.elon.edu/u/news/2025/03/12/survey-52-of-u-s-adults-now-use-ai-large-language-models-like-chatgpt/
+        <br>
+        Wei, J. Z., Wang, X., Schuurmans, D., Bosma, M., Chi, E. H., Le, Q. V., & Zhou, D. (2022). Chain-of-Thought Prompting Elicits Reasoning in Large Language Models. arXiv. https://doi.org/10.48550/arxiv.2201.11903
+        <br>
+        Yang, J., Jin, H., Tang, R., Han, X., Feng, Q., Jiang, H., Zhong, S., Yin, B., & Hu, X. (2024). Harnessing the Power of LLMs in Practice: A Survey on ChatGPT and Beyond. ACM Transactions on Knowledge Discovery from Data. https://doi.org/10.1145/3649506
+
+    </p>
 
 </div>
 
